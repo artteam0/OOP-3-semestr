6b3188312fca94404dffeecadcc488514fe86b1c
Artsiom <154960280+artteam0@users.noreply.github.com>
2024-12-28 00:55:28 +0300

    Add files via upload

---
+++ lab13 гс/lab13 (oop)/lab13 (oop)/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace lab13
+{
+    public interface ISerializer
+    {
+        void Serialize<T>(T obj, FileStream stream);
+        T Deserialize<T>(FileStream stream);
+    }
+
+    public class JsonSerializerWrapper : ISerializer
+    {
+        public void Serialize<T>(T obj, FileStream stream)
+        {
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+                WriteIndented = true
+            };
+            JsonSerializer.Serialize(stream, obj, options);
+        }
+
+        public T Deserialize<T>(FileStream stream)
+        {
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+                WriteIndented = true
+            };
+            return JsonSerializer.Deserialize<T>(stream, options);
+        }
+    }
+
+    public class XmlSerializerWrapper : ISerializer
+    {
+        public void Serialize<T>(T obj, FileStream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(stream, obj);
+        }
+
+        public T Deserialize<T>(FileStream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(stream);
+        }
+    }
+
+    [Serializable]
+    public class Cactus
+    {
+        public string Name { get; set; } = "Cactus";
+        public int Cost { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, cost: {Cost}";
+        }
+    }
+
+    class Program
+    {
+        static void TestSerialization(ISerializer serializer, Cactus cactus, string fileName)
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.Serialize(cactus, fileStream);
+            }
+            Cactus deserializedCactus;
+            using (var fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                deserializedCactus = serializer.Deserialize<Cactus>(fileStream);
+            }
+
+            Console.WriteLine($"Исходный объект: {cactus}");
+            Console.WriteLine($"Десериализованный объект: {deserializedCactus}");
+
+            Console.WriteLine($"Сериализовано в {fileName}.\n");
+        }
+
+        static void Main()
+        {
+            Cactus cactus = new Cactus { Name = "CACTUS", Cost = 100 };
+
+            TestSerialization(new JsonSerializerWrapper(), cactus, "Cactus.json");
+            Cactus deserializedCactusJson;
+            using (var fileStream = new FileStream("Cactus.json", FileMode.Open))
+            {
+                deserializedCactusJson = new JsonSerializerWrapper().Deserialize<Cactus>(fileStream);
+            }
+            Console.WriteLine("Десериализованный кактус из JSON:");
+            Console.WriteLine(deserializedCactusJson.ToString());
+
+            TestSerialization(new XmlSerializerWrapper(), cactus, "Cactus.xml");
+            Cactus deserializedCactusXml;
+            using (var fileStream = new FileStream("Cactus.xml", FileMode.Open))
+            {
+                deserializedCactusXml = new XmlSerializerWrapper().Deserialize<Cactus>(fileStream);
+            }
+            Console.WriteLine("Десериализованный кактус из XML:");
+            Console.WriteLine(deserializedCactusXml.ToString());
+
+            XElement cactusXml = new XElement("CACTUS",
+                new XElement("Cactus",
+                    new XElement("Name", "Кактус"),
+                    new XElement("Cost", 100)
+                ),
+                new XElement("cACTUS",
+                    new XElement("Name", "Кактусище"),
+                    new XElement("Cost", 100000)
+                )
+            );
+
+            cactusXml.Save("CactusLinq.xml");
+
+            Console.WriteLine("Все кактусы из XML:");
+            var allCactus = cactusXml.Elements("Cactus");
+            foreach (var cactusElement in allCactus)
+            {
+                Console.WriteLine(cactusElement.Element("Name")?.Value);
+            }
+        }
+    }
+}
---
+++ lab14 гс/lab14 (oop)/lab14 (oop)/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Threading;
+using System.ComponentModel.Design;
+
+namespace lab13
+{
+    class Program
+    {
+        static Mutex consoleMutex = new Mutex();
+        static ManualResetEvent pauseEvent = new ManualResetEvent(true);
+        static object lockObject = new object();
+        static bool evenTurn = false;
+        static Timer timer;
+        static void Main()
+        {
+            var allProcess = Process.GetCurrentProcess();
+            Console.WriteLine($"ID: {allProcess.Id}");
+            Console.WriteLine($"Name: {allProcess.ProcessName}");
+            Console.WriteLine($"Priority: {allProcess.PriorityClass}");
+            Console.WriteLine($"Start time: {allProcess.StartTime}");
+            Console.WriteLine($"Current state: {allProcess.Responding}");
+            Console.WriteLine($"All CPU time: {allProcess.TotalProcessorTime}");
+
+            AppDomain domain = AppDomain.CurrentDomain;
+            using (StreamWriter writer = new StreamWriter("task_2.txt"))
+            {
+                writer.WriteLine($"Имя домена: {domain.FriendlyName}");
+                writer.WriteLine($"Базовый директорий: {domain.BaseDirectory}");
+                writer.WriteLine("Выгруженные сборки: ");
+                foreach (var assembly in domain.GetAssemblies())
+                {
+                    writer.WriteLine($"- {assembly.FullName}");
+                }
+            }
+
+            ThirdTask();
+            EvenOddNumbersWithMutex();
+            Timer();
+
+            Console.WriteLine("Нажмите любую клавишу для завершения программы...");
+            Console.ReadKey();
+            timer.Dispose();
+        }
+
+        static void ThirdTask()
+        {
+            Console.WriteLine("Введите n (для управления потоком): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(() =>
+            {
+                using (StreamWriter writer = new StreamWriter("task_3.txt", true))
+                {
+                    for (int i = 2; i <= n; i++)
+                    {
+                        pauseEvent.WaitOne();
+                        if (isPrime(i))
+                        {
+                            consoleMutex.WaitOne();
+                            try
+                            {
+                                Console.WriteLine($"Значение: {i}");
+                                writer.WriteLine(i);
+                                Console.WriteLine($"Поток: {Thread.CurrentThread.Name}");
+                                Console.WriteLine($"Идентификатор: {Thread.CurrentThread.ManagedThreadId}");
+                                Console.WriteLine($"Приоритет: {Thread.CurrentThread.Priority}");
+                                Console.WriteLine($"Состояние: {Thread.CurrentThread.ThreadState}");
+                            }
+                            finally
+                            {
+                                consoleMutex.ReleaseMutex();
+                            }
+                        }
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+            thread.Start();
+            Thread.Sleep(750);
+            pauseEvent.Reset();
+            Console.WriteLine("Поток приостановлен");
+            Thread.Sleep(750);
+            pauseEvent.Set();
+            Console.WriteLine("Поток возобновлен");
+            thread.Join();
+        }
+
+        static bool isPrime(int i)
+        {
+            if (i < 2)
+            {
+                return false;
+            }
+
+            for (int j = 2; i <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void EvenOddNumbersWithMutex()
+        {
+            Console.Write("Введите n (для чётности - нечётности): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Thread evenThread = new Thread(() => PrintEvenNumbers(n)) { Priority = ThreadPriority.Highest };
+            Thread oddThread = new Thread(() => PrintOddNumbers(n)) { Priority = ThreadPriority.Lowest };
+
+            evenThread.Start();
+            evenThread.Join();
+
+            oddThread.Start();
+            oddThread.Join();
+
+            evenThread = new Thread(() => PrintEvenNumbersWithSync(n));
+            oddThread = new Thread(() => PrintOddNumbersWithSync(n));
+
+            oddThread.Start();
+            evenThread.Start();
+
+            oddThread.Join();
+            evenThread.Join();
+        }
+
+        static void PrintEvenNumbers(int n)
+        {
+            using (StreamWriter writer = new StreamWriter("task_4_1.txt"))
+            {
+                for (int i = 2; i <= n; i += 2)
+                {
+                    lock (lockObject)
+                    {
+                        writer.WriteLine(i);
+                        Console.WriteLine(i);
+                    }
+                    Thread.Sleep(75);
+                }
+            }
+        }
+
+        static void PrintOddNumbers(int n)
+        {
+            using (StreamWriter writer = new StreamWriter("task_4_2.txt"))
+            {
+                for (int i = 1; i <= n; i += 2)
+                {
+                    lock (lockObject)
+                    {
+                        writer.WriteLine(i);
+                        Console.WriteLine(i);
+                    }
+                    Thread.Sleep(125);
+                }
+            }
+        }
+
+        static void PrintEvenNumbersWithSync(int n)
+        {
+            for (int i = 2; i <= n; i += 2)
+            {
+                lock (lockObject)
+                {
+                    while (!evenTurn)
+                    {
+                        Monitor.Wait(lockObject);
+                    }
+
+                    Console.WriteLine($"Чередование (четное): {i}");
+                    evenTurn = false;
+                    Monitor.Pulse(lockObject);
+                }
+                Thread.Sleep(75);
+            }
+        }
+
+        static void PrintOddNumbersWithSync(int n)
+        {
+            for (int i = 1; i <= n; i += 2)
+            {
+                lock (lockObject)
+                {
+                    while (evenTurn)
+                    {
+                        Monitor.Wait(lockObject);
+                    }
+
+                    Console.WriteLine($"Чередование (нечетное): {i}");
+                    evenTurn = true;
+                    Monitor.Pulse(lockObject);
+                }
+                Thread.Sleep(125);
+            }
+        }
+
+        static void Timer()
+        {
+            timer = new Timer(state =>
+            {
+                string currTime = DateTime.Now.ToString("HH:mm:ss:fff");
+                Console.WriteLine($"Текущее время: {currTime}");
+                using (StreamWriter writer = new StreamWriter("Timer.txt", true))
+                {
+                    writer.WriteLine(currTime);
+                }
+            }, null, 0, 15);
+        }
+    }
+}
